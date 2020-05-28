023a3d632c66ed62931c29a1c92fd01e127c5043
1000copy <1000copy@gmail.com>
2020-05-28 18:54:19 +0800

    pure.js telnet

--- pure.js
+++ pure.js
@@ -1,5 +1,13 @@
-const net = require('net');
 var options = {host:"10.27.10.195"}
+var expects =[
+  {expect:/Username/,send:'zte'},
+  {expect:/Password/,send:'zte'},
+  {expect:/ZXAN/,send:'en'},
+  {expect:/Password/,send:'zxr10'},
+  {expect:/ZXAN#/,send:'con t'},
+  ]
+const net = require('net');
+
 let socketclient = net.createConnection({
       host: options.host,
       port: options.port || 23,
@@ -8,18 +16,16 @@ let socketclient = net.createConnection({
     })
     let step = 0;
     socketclient.on("data", function (data) {            
-      process.stdout.write(`${data}`.trim());
-      // auto login
-      if(data.includes('Username')){
-        socketclient.write('zte\n')
-        process.stdout.write(`zte`);        
-      }
-      if(data.includes('Password')){
-        socketclient.write('zte\n')
-        process.stdout.write(`zte`);        
+      process.stdout.write(`${data}`.trim());      
+      if(expects.length >= step + 1){
+        var item = expects[step]        
+        if(data.toString().match(item.expect)){          
+          socketclient.write(`${item.send}\n`)
+          process.stdout.write(`${item.send}\n`);
+          step++
+        }
       }
     });
-
     socketclient.on("close", function () {
       console.log('close');
     });
