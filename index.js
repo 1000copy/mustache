af1c2dbed609f755ccabb956d58cbd4517a0b7d6
1000copy <1000copy@gmail.com>
2020-05-29 11:30:41 +0800

    good begin

--- index.js
+++ index.js
@@ -6,44 +6,30 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.use(express.static('./'))
 var expressWs = require('express-ws')(app);
 var state = {}
-expressWs.getWss().on('connection',(ws) => {
-	if(buffer){
-		ws.send(buffer.toString())
-		buffer = undefined
-	}    
+var ws1 
+expressWs.getWss().on('connection',(ws) => {	
+	const net = require('net');
+	var t
+	var options = {host:"10.27.10.195",port:23}
+	let telnet = net.createConnection(options, function () {
+	  console.log('connected');
+	})
+	telnet.on("data", function (data) {            
+	  // process.stdout.write(`${data}`.trim())	  	  
+	  ws.send(data.toString())	  
+	});
+	telnet.on("close", function () {
+	  console.log('close');	  
+	});
+
+	telnet.on("error", function (err) {
+	  console.log(err);      
+	});
+	ws.telnet = telnet
 })
-app.ws('/echo', function(ws, req) {
-	
-  ws.on('message', function(msg) {
-  	console.log('from client:'+msg)
-    state.ws = ws// ws.send(msg);    
-    telnet.write(msg+'\r')
+app.ws('/echo', function(ws, req) {	
+  ws.on('message', function(msg) {  	
+    ws.telnet.write(msg+'\r')
   });
 });
-var options = {host:"10.27.10.195"}
-const net = require('net');
-var t
-var interval = 1000
-let telnet = net.createConnection({
-  host: options.host,
-  port: options.port || 23,
-}, function () {
-  console.log('connected');
-})
-telnet.on("data", function (data) {            
-  // process.stdout.write(`${data}`.trim())
-  if(state.ws)
-  	state.ws.send(data.toString())
-  else
-  	buffer = data
-});
-telnet.on("close", function () {
-  console.log('close');
-  process.exit()
-});
-
-telnet.on("error", function (err) {
-  console.log(err);      
-});
-
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
