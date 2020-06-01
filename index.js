655bf1ffeed59b6b5f8b292314e84c8f8623f498
1000copy <1000copy@gmail.com>
2020-06-01 10:33:33 +0800

    fix root to index.html

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.redirect('/index.html'))
 app.use(express.static('./'))
 var expressWs = require('express-ws')(app);
 var wson = require('./wson.js')
